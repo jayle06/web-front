af417a979621b2cfcdc46cd6f80e8c22d9245c16
jayle06 <mhoang0611@gmail.com>
2021-02-20 21:26:16 +0700

    init

---
+++ src/Header.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import logo from './images/white-logo.png';
+import './Header.css';
+import {Link} from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
+import LocalMallIcon from '@material-ui/icons/LocalMall';
+
+function Header() {
+    return (
+        <nav className="header">
+            {/* logo */}
+            <Link to="/">
+                <img className="header__logo" src= {logo} alt="logo"/>
+            </Link>
+            
+            {/* search box */}
+            <div className="header__search">
+                <input type="text" className="header__searchInput"/>
+                <SearchIcon className="header__searchIcon"/>
+            </div>
+            {/* 3 links */}
+            <div className="header__nav">
+                <Link to="/login" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Hello, </span>
+                        <span className="header__optionLineTwo">Sign In</span>
+                    </div>
+                </Link>
+
+                <Link to="/" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Return </span>
+                        <span className="header__optionLineTwo">@ Order</span>
+                    </div>
+                </Link>
+
+                <Link to="/" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Your </span>
+                        <span className="header__optionLineTwo">Prime</span>
+                    </div>
+                </Link>
+
+                <Link to="/checkout" className="header__link">
+                    <div className="header__optionBasket">
+                        <LocalMallIcon />
+                        <span>0</span>
+                    </div>
+                </Link>
+            </div>
+            
+            {/* cart icon */}
+        </nav>
+    )
+}
+
+export default Header
